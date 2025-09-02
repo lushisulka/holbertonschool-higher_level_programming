5afc9f85f0fef925bc526eabb02d76381d33889e
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 16:49:40 +0200

    Check task 3 in SQL-More Queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Ensure that the table is created only if it does not already exist
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT = 1;
+    id INT DEFAULT 1,
     name VARCHAR(100) NOT NULL
 )
