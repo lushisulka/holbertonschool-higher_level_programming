4640758d8aaee5ce766b1e798185f908dabe4337
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 16:48:09 +0200

    Check task 3 in SQL-More Queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- Write a SQL query to create a table named `id_not_null` with two columns: `id` of type INT with a default value of 1, and `name` of type VARCHAR(100) that cannot be NULL.
+-- Ensure that the table is created only if it does not already exist
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT = 1;
     name VARCHAR(100) NOT NULL
