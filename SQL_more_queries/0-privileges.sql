d2a4cacd98b33f9a9cdb3046131ef33f5b2d8986
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 15:55:50 +0200

    Check task 0 in SQL-More Queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- script to list all records in the two tables
+-- write a script that prints all privileges of user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-SHOW GRANTS FOR  user_0d_1, user_0d_2;
+-- write a script that prints all privileges of user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
