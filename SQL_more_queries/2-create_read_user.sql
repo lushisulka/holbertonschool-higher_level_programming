3f40ffcb2d697741e519efe00f504d725a0f17e0
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 16:35:03 +0200

    Check task 0 in SQL-More Queries

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- write a script that creates the user user_0d_2
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2'@'user_0d_2';
+-- write a script that gives all privileges to user_0d_2 on the database hbtn_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'hbtn_0d_2'@'user_0d_2';
