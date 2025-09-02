a0b3405ff44a0716c0c81e9aad4fddd583bfa139
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 13:51:54 +0200

    check exercise in sql

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,3 @@
+--script to list all databases
+
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,1 +1,3 @@
+--script to list all databases
+
 INSERT INTO hbtn_0c_0.table_name (`id`, `name`) VALUES ('89', 'Best School');
