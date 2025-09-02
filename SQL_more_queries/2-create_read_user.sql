1211414ce938888f8128996406e17aff4cd01b4b
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-02 16:36:54 +0200

    Check task 0 in SQL-More Queries

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,8 @@
+-- write a script that creates the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+
 -- write a script that creates the user user_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2'@'user_0d_2';
--- write a script that gives all privileges to user_0d_2 on the database hbtn_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'hbtn_0d_2'@'user_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- write a script that gives SELECT privilege to user_0d_2 on the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
