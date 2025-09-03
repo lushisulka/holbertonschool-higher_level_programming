8760c8bf065dceabeb969480768853675b8b30fa
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 16:26:56 +0200

    check task 0 in mysql-advanced

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create a table named 'users' with the following columns:
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) 
+);
