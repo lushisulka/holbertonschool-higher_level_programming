fedf0a8bd7f1a2b2152080c89fd8d6fa428aa125
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 14:30:28 +0200

    add script to create table

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,9 +2,17 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
 -- USE the database
 USE hbtn_0d_tvshows;
+
+-- Create the tv_shows table
+CREATE TABLE `tv_shows` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
 -- Query to list all TV shows with their genre IDs
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+INNER JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
