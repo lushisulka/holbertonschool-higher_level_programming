54678e7ccbb906843ae8bedca450fd67e9d779b3
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 14:12:49 +0200

    check task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,5 @@
--- Create the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
--- USE the database
-USE hbtn_0d_tvshows;
 -- Query to list all TV shows with their genre IDs
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows
-    INNER JOIN tv_show_genres 
-       ON tv_shows.id = tv_show_genres.tv_show_id
-    ORDER BY tv_shows.title, tv_show_genres.genre_id;
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
