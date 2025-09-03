7e4bed425854fe1129d7a35580c556eb6c5236a3
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 13:53:41 +0200

    check task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,9 @@
-
-
+-- Query to list all TV shows with their genre IDs
+CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
+-- USE the database
+USE hbtn_0d_tvshows;
+-- Query to list all TV shows with their genre IDs
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.id ASC;
