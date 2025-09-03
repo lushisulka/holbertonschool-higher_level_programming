54066781162686ea63a2554b80b22f364774f301
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 13:21:30 +0200

    check task 6

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create the database only if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Switch to the database
+USE hbtn_0d_usa;
+-- Ensure that the table is created only if it does not already exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+    name VARCHAR(256) NOT NULL,
+);
