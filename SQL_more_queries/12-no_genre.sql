2e7275846da86de4b79e1a88556196963ff79562
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 15:29:48 +0200

    check task 11

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- Query to list all TV shows with their genre IDs
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
