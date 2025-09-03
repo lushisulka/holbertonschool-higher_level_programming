c3004f06b4bac7036ef986a6c904c414752230c3
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 15:17:28 +0200

    check task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,24 +1,5 @@
--- Create the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
--- USE the database
-USE hbtn_0d_tvshows;
-
--- Create the tv_shows table
-CREATE TABLE `tv_show_genres` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
--- Create the tv_shows table
-CREATE TABLE `tv_shows` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
 -- Query to list all TV shows with their genre IDs
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
