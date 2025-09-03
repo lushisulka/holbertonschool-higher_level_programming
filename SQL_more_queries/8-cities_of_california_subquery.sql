adfb82102087f6a7930e15b0e16182d79c1d036e
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 13:36:39 +0200

    check task 9

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,5 @@
--- Query to list all cities in California using a join
+-- Query to list all cities in California without using a join
 
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state_id = states.id
-  AND states.name = 'California'
+SELECT cities.id, cities.name FROM cities, states
+WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
