4db8c14f152d546514c34b4be8569b5ab9270c40
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 13:28:50 +0200

    check task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- Write a SQL query to list all the cities of California in alphabetical order
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+SELECT cities.name AS city FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY city;
