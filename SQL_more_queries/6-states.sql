6f9ff68ab199b2f1d27b13621071bcfe1e2e5c0c
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-03 13:18:22 +0200

    check task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Create the database only if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 -- Ensure that the table is created only if it does not already exist
 CREATE TABLE IF NOT EXISTS states (
     id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
