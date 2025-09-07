5c4d437a9fc6c712f5dca8c22c4e7e22e11d6f0a
lushisulka <sulkaengjellushe5@gmail.com>
2025-09-07 22:11:03 +0200

    Completed all project

--- MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,6 +0,0 @@
--- Create a table named 'users' with the following columns:
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) 
-);
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Create the database only if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Switch to the database
+-- USE to the database
 USE hbtn_0d_usa;
 -- Ensure that the table is created only if it does not already exist
 CREATE TABLE IF NOT EXISTS cities (
